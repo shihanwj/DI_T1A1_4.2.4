a5b5aa35df8721741e5a10338393860c613f768e
shihanwj <shihanwei495@gmail.com>
2024-10-03 11:25:06 +0200

    Delete ColorfulCircles directory

--- ColorfulCircles/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.colorfulcircles {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.colorfulcircles to javafx.fxml;
-    exports es.ieslosmontecillos.colorfulcircles;
-}
